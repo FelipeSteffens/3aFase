31e703b0522f807bd9f66b22c60c675dae5d16ab
FelipeSteffens <felipesteffens2019@outlook.com>
2026-02-24 22:12:05 -0300

    começando a colocar o card no body

--- front-end/fundamentos/src/layouts/Body/index.jsx
+++ front-end/fundamentos/src/layouts/Body/index.jsx
@@ -2,9 +2,28 @@ import Card from '../../components/Cards/Card'
 import styles from './Body.module.css'
 
 const Body = () => {
+
+    const usuarios = [
+        {nome: "Ana", idade: 22, cidade:"São josé"},
+        {nome: "Felipe", idade: 19, cidade:"Floripa"},
+        {nome: "Karla", idade: 18, cidade:"São josé"},
+    ]
+
   return (
     <>
-       <h1>Body</h1>
+       <main className={styles.body}>
+            <h2>Usúarios cadastrados</h2>
+            <div className={styles.cardContainer}>
+                {usuarios.map((usuario, index) => (
+                    <Card
+                        key={index}
+                        nome={usuario.nome}
+                        idade={usuario.idade}
+                        cidade={usuario.cidade}
+                    />
+                ))}
+            </div>
+       </main>
     </>
   )
 }
