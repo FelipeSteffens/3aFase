17edf2f715c86b7db0fa777d843c11f73bef0c6f
FelipeSteffens <felipesteffens2019@outlook.com>
2026-03-03 21:48:43 -0300

    card api

---
+++ front-end/fundamentos/src/components/Cards/CardApi.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+import styles from "./card.module.css"
+
+export const CardApi = () => {
+  // // let contador = 0
+  // const [contador, setContador] = useState(0)
+
+  // const incrementaValor = () => {
+  //   // contador++
+  //   setContador((prev) => prev + 1)
+  //   console.log("contador", contador)
+  // }
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+  }, [])
+
+  return (
+    <>
+      <div className={styles.cardContainerApi}>
+        {users.map((user) => (
+          <div className={styles.card} key={user.id}>
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+            <p>{user.address.street}</p>
+            <p>{user.address.geo.lat}</p>
+            <p>{user.address.geo.lng}</p>
+          </div>
+        ))}
+      </div>
+      {/* <p>{contador}</p>
+      <button onClick={incrementaValor}>add</button> */}
+    </>
+  )
+}
--- front-end/fundamentos/src/layouts/Body/index.jsx
+++ front-end/fundamentos/src/layouts/Body/index.jsx
@@ -1,29 +1,32 @@
-import Card from '../../components/Cards/Card'
-import styles from './Body.module.css'
+import Card from "../../components/Cards/Card"
+import { CardApi } from "../../components/Cards/CardApi"
+import styles from "./Body.module.css"
 
 const Body = () => {
-
-    const usuarios = [
-        {nome: "Ana", idade: 22, cidade:"São josé"},
-        {nome: "Felipe", idade: 19, cidade:"Floripa"},
-        {nome: "Karla", idade: 18, cidade:"São josé"},
-    ]
+  const usuarios = [
+    { nome: "Ana", idade: 22, cidade: "São josé" },
+    { nome: "Felipe", idade: 19, cidade: "Floripa" },
+    { nome: "Karla", idade: 18, cidade: "São josé" },
+  ]
 
   return (
     <>
-       <main className={styles.body}>
-            <h2>Usúarios cadastrados</h2>
-            <div className={styles.cardContainer}>
-                {usuarios.map((usuario, index) => (
-                    <Card
-                        key={index}
-                        nome={usuario.nome}
-                        idade={usuario.idade}
-                        cidade={usuario.cidade}
-                    />
-                ))}
-            </div>
-       </main>
+      <main className={styles.body}>
+        <h2>Usúarios cadastrados</h2>
+        <div className={styles.cardContainer}>
+          {usuarios.map((usuario, index) => (
+            <Card
+              key={index}
+              nome={usuario.nome}
+              idade={usuario.idade}
+              cidade={usuario.cidade}
+            />
+          ))}
+        </div>
+
+        <h3>Usuários vindos da API</h3>
+        <CardApi />
+      </main>
     </>
   )
 }
